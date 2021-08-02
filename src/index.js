1ebdb424a6c78a8959e0af407cb433a587d9aa8c
Antonio <rapozoantonio@gmail.com>
2021-08-02 14:14:58 -0300

    feat: adjust root

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import * as serviceWorker from "./serviceWorker";
 import "./assets/themify-icon/themify-icons.css";
 import "./assets/simple-line-icon/simple-line-icons.css";
